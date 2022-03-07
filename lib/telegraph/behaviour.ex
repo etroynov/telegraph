b0bd861700cf9db5705ce95d5f989384dca7d8f6
Evgenii Troinov <troinof@gmail.com>
2022-03-07 19:57:06 +0100

    feat(payments): add payments functions

--- lib/telegraph/behaviour.ex
+++ lib/telegraph/behaviour.ex
@@ -46,4 +46,19 @@ defmodule Telegraph.Behaviour do
               {:ok, Message.t()} | {:error, Error.t()}
   @callback answer_inline_query(binary, [Telegraph.Model.InlineQueryResult.t()], [{atom, any}]) ::
               :ok | {:error, Error.t()}
+  @callback send_invoice(
+              integer | binary,
+              binary,
+              binary,
+              binary,
+              binary,
+              binary,
+              binary,
+              [Nadia.Model.LabeledPrice.t()],
+              [{atom, any}]
+            ) :: {:ok, Message.t()} | {:error, Error.t()}
+  @callback answer_shipping_query(integer | binary, boolean, [{atom, any}]) ::
+              :ok | {:error, Error.t()}
+  @callback answer_pre_checkout_query(integer | binary, boolean, [{atom, any}]) ::
+              :ok | {:error, Error.t()}
 end
